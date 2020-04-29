e7cf9e2bb0c455ab015a156ceb5d000b3f69e76d
coder111 <coder111@protonmail.com>
2020-04-30 00:37:43 +0100

    Rebase to 1.9. Reapply entire mini changelog. Fix ogg conversion and rename.

--- src/convert_to_ogg.sh
+++ src/convert_to_ogg.sh
@@ -1,3 +1,4 @@
 find . -name "*.wav" | xargs -n1 -P7 oggenc
 
-find . -name "audio.txt" -exec sed -i.bak s/\\.wav/\\.ogg/ {} \;
+find . -name "audio_music.txt" -exec sed -i.bak s/\\.wav/\\.ogg/ {} \;
+find . -name "audio_sounds.txt" -exec sed -i.bak s/\\.wav/\\.ogg/ {} \;
